dd3a309f8994f81b03731adce988ec1c3b0cd152
AbadakiGideon <meetgideon4@gmail.com>
2025-07-21 03:41:29 -0700

    Add files via upload

---
+++ swiftapply25/pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Welcome to SwiftApply AI</h1>
+      <p>This is a working Next.js template ready for deployment on Vercel.</p>
+    </div>
+  )
+}
